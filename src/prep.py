0fc69c369e1b1352ebddd25080858514315ed8fc
Бадертдинов Ибрагим <badertdinov.i.i@sberbank.ru>
2021-02-08 22:06:58 +0300

    ru_data

---
+++ src/prep.py
@@ -0,0 +1,66 @@
+import string
+
+punc_set = {k:v for v,k in enumerate(string.punctuation)}
+
+PNC = {',':'PERIOD','.':'COMMA',
+'QUESTION':'?'}
+pnc = ',.?'
+punc_set = {k:v for v,k in zip(list(range(1,len(pnc))), pnc)}
+extra_punc = list(set(string.punctuation) - set(pnc))
+sps = [' ' + p for p in pnc]
+
+import re, string
+pattern = re.compile('[^a-zA-ZА-Яа-я0-9,?.\s]+')
+
+
+def clean_extra(s):
+    s = s.replace('!', '.')
+    s = s.replace('...', '.')
+    s = s.replace(':', ',')
+    s = s.replace(';', ',')
+    # for i in extra_punc:
+    #     s = s.replace(i, '')
+    s = pattern.sub('', s)
+    l = s.split()
+    for i,w in enumerate(l):
+      if l[i] in pnc:
+        p = l.pop(i)
+        l[i-1] = l[i-1] + p
+    return " ".join(l)
+
+def get_targets(s):
+    ids = []
+    for w in s:
+        p = PNC.get(w[-1], 'O') 
+        ids.append(p)
+    return ids
+
+def clean_targets(sl):
+    al = []
+    for s in sl:
+        s = s.lower()
+        for i in pnc:
+            s = s.replace(i, '')
+        al.append(s)
+    return al
+
+import pandas as pd
+
+FN = '/Users/ibragim/Documents/ytsubs/ru-ted.txt'
+with open(FN) as f: txt = f.read()
+
+txt = clean_extra(txt)
+tl = txt.split()
+y = get_targets(tl)
+x = clean_targets(tl)
+
+
+df = pd.DataFrame({'text': x, 'label': y})
+
+
+from sklearn.model_selection import train_test_split
+
+train, test = train_test_split(df, test_size=0.1)
+
+train.to_csv('data/train_ru.csv', index=False)
+test.to_csv('data/test_ru.csv', index=False)
