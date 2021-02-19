278c9e44f047a57cbf0c99faa9ffde6169b5af46
Бадертдинов Ибрагим <badertdinov.i.i@sberbank.ru>
2021-02-19 19:40:49 +0300

    f

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 python src/new_ru_train.py --cuda=False --pretrained-model=DeepPavlov/rubert-base-cased-conversational --freeze-bert=False --lstm-dim=-1 \
 --language=ru --seed=1 --lr=5e-5 --epoch=3 --use-crf=False --augment-type=all  --augment-rate=0.15 --batch-size=32 \
---alpha-sub=0.4 --alpha-del=0.4 --data-path=data/ru/**/*.txt --save-path=out
+--alpha-sub=0.4 --alpha-del=0.4 --data-path=data/ru/**/**/*.txt --save-path=out
 # python src/train.py --cuda=True --pretrained-model=sberbank-ai/sbert_large_nlu_ru --freeze-bert=False --lstm-dim=-1 \
 # --language=ru --seed=1 --lr=5e-5 --epoch=3 --use-crf=False --augment-type=all  --augment-rate=0.15 --batch-size=12 \
 # --alpha-sub=0.4 --alpha-del=0.4 --data-path=data --save-path=out --sequence-length 92
