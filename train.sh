742da1e514ada8545dac34417bb8d12a1053a699
Бадертдинов Ибрагим <badertdinov.i.i@sberbank.ru>
2021-05-14 16:09:08 +0300

    test update

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
-python src/new_ru_train.py --cuda=True --pretrained-model=DeepPavlov/rubert-base-cased --freeze-bert=False --lstm-dim=-1 \
---language=ru --seed=1 --lr=2e-5 --epoch=22 --use-crf=False --augment-type=all  --augment-rate=0.05 --batch-size=64 \
---alpha-sub=0.4 --alpha-del=0.4 --data-path=data/ru/*.txt --save-path=out --sequence-length 128 --yttm yttm.model --pqrnn=True #--hg True
+python src/new_ru_train.py --cuda=True --pretrained-model=DeepPavlov/rubert-base-cased --freeze-bert=False --lstm-dim=512 \
+--language=ru --seed=1 --lr=1e-5 --epoch=16 --use-crf=False --augment-type=all  --augment-rate=0.01 --batch-size=16 \
+--alpha-sub=0.4 --alpha-del=0.4 --data-path=data/ru/*.txt --save-path=out --sequence-length 256 # --yttm yttm.model --pqrnn=False #--hg True
 # python src/new_ru_train.py --cuda=False --pretrained-model=DeepPavlov/rubert-base-cased- --freeze-bert=False --lstm-dim=-1 \
 # --language=ru --seed=1 --lr=0.001 --epoch=20 --use-crf=False --augment-type=all  --augment-rate=0.0 --batch-size=256 \
 # --alpha-sub=0.4 --alpha-del=0.4 --data-path=data/ru/*.txt  --save-path=out --sequence-length 92 --yttm yttm.model #--pqrnn=True
