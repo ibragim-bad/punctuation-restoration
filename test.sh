1246bccbfdac143ce5a4aa9ad07ff6cf0d449cf6
Бадертдинов Ибрагим <badertdinov.i.i@sberbank.ru>
2021-05-14 16:57:03 +0300

    prep fix

--- test.sh
+++ test.sh
@@ -5,6 +5,6 @@
 # python3 src/inference.py --pretrained-model=prajjwal1/bert-small --language=en --cuda=false \
 # --in-file=data/test_en.txt --out-file=data/test_en_out.txt
 python3 src/inference.py --pretrained-model=DeepPavlov/rubert-base-cased --weight-path=out/weights.pt --language=ru --cuda=false \
---in-file=data/test_ru.txt --out-file=data/test_ru_out.txt #--yttm yttm.model
+--in-file=data/test_ru.txt --out-file=data/test_ru_out.txt  --lstm-dim=512 #--yttm yttm.model
 # python3 src/inference.py --pretrained-model=DeepPavlov/rubert-base-cased-conversational --weight-path=out/st_weights.pt --language=ru --cuda=false \
 # --in-file=data/test_ru.txt --out-file=data/test_ru_out.txt  --sequence-length 128
